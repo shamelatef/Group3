13f220d83dfc6ae241ad7012717fda9d8c703500
Shamel <shamelatef@github.com>
2022-11-28 20:27:14 +0200

    New GUI

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ window.geometry("1920x1080")
 
 tabControl = ttk.Notebook(window)
 tab1 = ttk.Frame(tabControl)
+#tab2 = ttk.Frame(tabControl)
 tab2 = ttk.Frame(tabControl)
-tab3 = ttk.Frame(tabControl)
 
-tabControl.add(tab1, text ='SIP')
-tabControl.add(tab2, text ='Extensions')
-tabControl.add(tab3, text ='Logs')
+tabControl.add(tab1, text ='Add Agents')
+#tabControl.add(tab2, text ='Extensions')
+tabControl.add(tab2, text ='Logs')
 
 tabControl.pack(expand = 1, fill ="both")
 
@@ -33,64 +33,76 @@ label = Label(
     image=img
 )
 
-label2 = Label(
-    tab2,
-    image=img
-)
+
 
 label3 = Label(
-    tab3,
+    tab2,
     image=img
 )
 
 
 
 label.place(x=0, y=0)
-label2.place(x=0,y=0)
+#label2.place(x=0,y=0)
 label3.place(x=0, y=0)
 
 #################################################
 
-pygame.mixer.init()
+#pygame.mixer.init()
 
 
 window.title("Sayyad's CallCenter")
 
 Name=StringVar()
 Type=StringVar()
-Context=StringVar()
+Context="Agent"
 Secret=StringVar()
 Host="dynamic"
 Disallow="all"
 Allow="ulaw"
 MailBox=StringVar()
-
-#Extension tab
 Extension=StringVar()
-Callerid=StringVar()
-Context2=StringVar()
+
+
+
+options = [
+ "AR Q","EN Q", "FR Q"
+]
+  
+clicked = StringVar()
+  
+clicked.set( "AR Q" )
+  
+# Create Dropdown menu
+drop = OptionMenu( tab1 , clicked , *options ).place(x=350,y=150)
+
+# Create button, it will change label text
 
 
 def submit():
     NewName=Name.get()
     NewType=Type.get()
-    NewContext=Context.get()
+    #NewContext="Agent"
     NewSecret=Secret.get()
+    NewExtension=Extension.get()
+
+
+  
 
     NewMailBox=MailBox.get()
+    label.config( text = clicked.get() )
+
     msg = ''
 
-    if (len(NewName) == 0 or len(NewType) == 0 or len(NewContext) == 0 or len(NewSecret) == 0  or len(NewMailBox)== 0):
+    if (len(NewName) == 0 or len(NewType) == 0 or  len(NewSecret) == 0  or len(NewMailBox)== 0):
             msg = 'At least one label field is empty'
     else:
             try:
                 
                 if (NewType != "friend" and NewType != "peer" and NewType != "user"):
                     msg = "Wrong Type"
-                elif ((NewContext != "Agent") and (NewContext != "incoming") and (NewContext != "Manager")):
-                    msg='You can only add employees in contexts (Manager, Agent, incoming)'
                 else:
-                    
+#SIP.conf          
                  with open(r'sayyad.txt', 'r+') as f:
                     data=f.read()
                     if ("["+NewName+"]") in data:
@@ -99,30 +111,96 @@ def submit():
                         
                     else:
 
+                        f =open("sayyad.txt","a")
                         f.write("[" + NewName +"]\n")
                         f.write("type="+NewType +"\n")
-                        f.write("context="+NewContext +"\n")
+                        f.write("context="+Context +"\n")
                         f.write("secret="+NewSecret +"\n")
                         f.write("host="+Host +"\n")
                         f.write("disallow="+Disallow+"\n")
                         f.write("allow="+Allow +"\n")
                         f.write("mailbox="+NewMailBox +"@VoiceMail\n\n")
+#Extensions.conf
+                        with open(r'sayyad.txt', 'r+') as f:
+                            data = f.read()
+                            if ("[Agent]") in data:
+                             data = data.replace("[Agent]", "[Agent]\n" + "exten => "+NewExtension +",Dial(SIP/"+NewName+")\n" + "same => n,Hangup()\n\n")
+                        
+                        with open(r'sayyad.txt', 'w') as file:
+                            file.write(data)
+
+                        with open(r'sayyad.txt', 'r+') as f:
+                            data = f.read()
+                            if ("[Manager]") in data:
+                                data = data.replace("[Manager]", "[Manager]\n"+ "exten => "+NewExtension +",Dial(SIP/"+NewName+")\n" + "same => n,Hangup()\n\n")
+                        
+                        with open(r'sayyad.txt', 'w') as file:
+                            file.write(data)
+
+                        with open(r'sayyad.txt', 'r+') as f:
+                            data = f.read()
+                            if ("[CEO]") in data:
+                                data = data.replace("[CEO]", "[CEO]\n"+ "exten => "+NewExtension +",Dial(SIP/"+NewName+")\n" + "same => n,Hangup()\n\n")
+                        
+                        with open(r'sayyad.txt', 'w') as file:
+                            file.write(data) 
+
+                        with open(r'sayyad.txt', 'r+') as f:
+                            data = f.read()
+                            if ("[Tech-Sup]") in data:
+                                data = data.replace("[Tech-Sup]", "[Tech-Sup]\n"+ "exten => "+NewExtension +",Dial(SIP/"+NewName+")\n" + "same => n,Hangup()\n\n")
+                        with open(r'sayyad.txt', 'w') as file:
+                            file.write(data)  
+
+#Queues.conf
+
+                        if (clicked.get()=="EN Q"):
+                            print("HelloWorld")
+                            with open(r'sayyad.txt', 'r+') as f:
+                              data = f.read()
+                              if ("[en]") in data:
+                                    data = data.replace("[en]", "[en]\n"+ "member => SIP/"+NewName+"\n" )
+                            with open(r'sayyad.txt', 'w') as file:
+                              file.write(data)  
+                        elif (clicked.get()=="AR Q"):
+                            print("HelloWorld")
+                            with open(r'sayyad.txt', 'r+') as f:
+                              data = f.read()
+                              if ("[ar]") in data:
+                                    data = data.replace("[ar]", "[ar]\n"+ "member => SIP/"+NewName+"\n" )
+                            with open(r'sayyad.txt', 'w') as file:
+                              file.write(data)  
+                        if (clicked.get()=="FR Q"):
+                            print("HelloWorld")
+                            with open(r'sayyad.txt', 'r+') as f:
+                              data = f.read()
+                              if ("[fr]") in data:
+                                    data = data.replace("[fr]", "[fr]\n"+ "member => SIP/"+NewName+"\n" )
+                            with open(r'sayyad.txt', 'w') as file:
+                              file.write(data)  
+
+
+
+
+                      
+                       
                         f.close()
                         Name.set("")
                         Type.set("")
-                        Context.set("")
+                        #Context.set("")
                         Secret.set("")
+                        Extension.set("")
                         MailBox.set("")
                         msg = 'Success!'
-                        pygame.mixer.music.load("waa.mp3")
-                        pygame.mixer.music.play()
+                        #pygame.mixer.music.load("waa.mp3")
+                        #pygame.mixer.music.play()
+
             except Exception as ep:
                 messagebox.showerror('error', ep)
 
     messagebox.showinfo('message', msg)
 
 
-# GUI --> only can add existing contexts (incoming / Agent / Manager)
     
 
     
@@ -140,123 +218,25 @@ name_entry = Entry(tab1,textvariable = Name, font=('calibre',10,'normal'),bg="bl
 type_label = Label(tab1, text = 'type=', font=('calibre',10, 'bold'),bg="white",foreground="black").place(x=200,y=110)
 type_entry = Entry(tab1,textvariable = Type, font=('calibre',10,'normal'),bg="black",foreground="white").place(x=300,y=110)
 
-context_label = Label(tab1, text = 'context=', font=('calibre',10, 'bold'),bg="white",foreground="black").place(x=200,y=150)
-context_entry = Entry(tab1,textvariable = Context, font=('calibre',10,'normal'),bg="black",foreground="white").place(x=300,y=150)
+Queue_label = Label(tab1, text = 'QueueName', font=('calibre',10, 'bold'),bg="white",foreground="black").place(x=200,y=150)
 
 secret_label = Label(tab1, text = 'secret=', font=('calibre',10, 'bold'),bg="white",foreground="black").place(x=200,y=190)
 secret_entry = Entry(tab1,textvariable = Secret, font=('calibre',10,'normal'),show='*',bg="black",foreground="white").place (x=300,y=190)
-"""
-host_label = Label(window, text = 'host=', font=('calibre',10, 'bold'),bg="black",foreground="white").place(x=150,y=230)
-host_entry = Entry(window,textvariable = Host, font=('calibre',10,'normal'),bg="black",foreground="white").place(x=250,y=230)
-
-disallow_label = Label(window, text = 'disallow=', font=('calibre',10, 'bold'),bg="black",foreground="white").place(x=150,y=270)
-disallow_entry = Entry(window,textvariable = Disallow, font=('calibre',10,'normal'),bg="black",foreground="white").place(x=250,y=270)
 
 
-allow_label = Label(window, text = 'allow=', font=('calibre',10, 'bold'),bg="black",foreground="white").place(x=150,y=310)
-allow_entry = Entry(window,textvariable = Allow, font=('calibre',10,'normal'),bg="black",foreground="white").place(x=250,y=310)
-"""
 mailbox_label = Label(tab1, text = 'mailbox=', font=('calibre',10, 'bold'),bg="white",foreground="black").place(x=200,y=230)
 mailbox_entry = Entry(tab1,textvariable = MailBox, font=('calibre',10,'normal'),bg="black",foreground="white").place(x=300,y=230)
+
+exten_label = Label(tab1, text = 'Extension', font=('calibre',10, 'bold'),bg="white",foreground="black").place(x=200,y=270)
+exten_entry = Entry(tab1,textvariable = Extension, font=('calibre',10,'normal'),bg="black",foreground="white").place(x=300,y=270)
 
 submit_button=Button(tab1,text = 'Submit', command = submit,bg="white").place(x=300,y=340)
 
 
-##############################
 
 
 
 #TAB - 2
-
-Header2 = Label(tab2,text = "Simple Dial configuration in Extensions.conf",font=('calibre',10, 'bold'),bg="white",foreground="black").place(x=180,y=10)
-
-def submit2():
-    NewContext2=Context2.get()
-    NewExtension=Extension.get()
-    NewCallerid=Callerid.get()
-    msg = ''
-    replace_text = "[" + NewContext2 +"]\n" + "exten => "+NewExtension +",Dial(SIP/"+NewCallerid+")\n" + "same => n,Hangup()\n\n"
-    search = "["+NewContext2+"]"
-    #print(replace_text)
-    if (len(NewContext2) == 0 or len(NewExtension) == 0 or len(NewCallerid) == 0):
-            msg = 'At least one label field is empty'
-    else:
-            try:              
-                 if (NewContext2=="Agent"):
-                     with open(r'sayyad.txt', 'r+') as f:
-                        data = f.read()
-                        if (search) in data:
-                         data = data.replace(search, replace_text)
-                        
-                     with open(r'sayyad.txt', 'w') as file:
-                        file.write(data)
-
-                     with open(r'sayyad.txt', 'r+') as f:
-                        data = f.read()
-                        if ("[Manager]") in data:
-                         data = data.replace("[Manager]", "[Manager]\n"+ "exten => "+NewExtension +",Dial(SIP/"+NewCallerid+")\n" + "same => n,Hangup()\n\n")
-                        
-                     with open(r'sayyad.txt', 'w') as file:
-                        file.write(data)
-
-                     with open(r'sayyad.txt', 'r+') as f:
-                        data = f.read()
-                        if ("[CEO]") in data:
-                         data = data.replace("[CEO]", "[CEO]\n"+ "exten => "+NewExtension +",Dial(SIP/"+NewCallerid+")\n" + "same => n,Hangup()\n\n")
-                        
-                     with open(r'sayyad.txt', 'w') as file:
-                        file.write(data) 
-
-                     with open(r'sayyad.txt', 'r+') as f:
-                        data = f.read()
-                        if ("[Tech-Sup]") in data:
-                         data = data.replace("[Tech-Sup]", "[Tech-Sup]\n"+ "exten => "+NewExtension +",Dial(SIP/"+NewCallerid+")\n" + "same => n,Hangup()\n\n")
-                        
-                     with open(r'sayyad.txt', 'w') as file:
-                        file.write(data)            
-
-                 f.close()
-                 Context2.set("")
-                 Extension.set("")
-                 Callerid.set("")
-                
-                 msg = 'Success!'
-                 pygame.mixer.music.load("waa.mp3")
-                 pygame.mixer.music.play()
-            except Exception as ep:
-                messagebox.showerror('error', ep)
-
-    messagebox.showinfo('message', msg)
-
-"""
-    with open(r'sayyad.txt', 'r+') as file:
-
-    # Reading the content of the file
-    # using the read() function and storing
-    # them in a new variable
-    data = file.read()
-
-    # Searching and replacing the text
-    # using the replace() function
-    if 'blabla' in data:
-        data = data.replace(search_text, replace_text)
-    else:
-        file.write(replace_text)
-"""
-
-context2_label = Label(tab2, text = 'context', font=('calibre',10, 'bold'),bg="white",foreground="black").place(x=200,y=70)
-context2_entry = Entry(tab2,textvariable = Context2, font=('calibre',10,'normal'),bg="black",foreground="white").place(x=300,y=70)
-
-ext_label = Label(tab2, text = 'Extension', font=('calibre',10, 'bold'),bg="white",foreground="black").place(x=200,y=110)
-ext_entry = Entry(tab2,textvariable = Extension, font=('calibre',10,'normal'),bg="black",foreground="white").place(x=300,y=110)
-
-callerid_label = Label(tab2, text = 'Caller ID', font=('calibre',10, 'bold'),bg="white",foreground="black").place(x=200,y=150)
-callerid_entry = Entry(tab2,textvariable = Callerid, font=('calibre',10,'normal'),bg="black",foreground="white").place(x=300,y=150)
-
-submit_button2=Button(tab2,text = 'Submit', command = submit2,bg="white").place(x=300,y=240)
-
-
-#TAB - 3
 
 def openCallLogFunc():
     filename1 ="/var/log/asterisk/cdr-csv/Master.csv"
@@ -276,9 +256,9 @@ def openQueue_Func():
     subprocess.call([opener, filename3])
     #Keep_both_files_in_the_same_Folder
 
-b1=tk.Button(tab3, text="Call Log",bg="white",command=openCallLogFunc)
-b2=tk.Button(tab3, text="Complaints Log",bg="white",command=openComplaintsFunc)
-b3=tk.Button(tab3, text="Queue Log",bg="white",command=openQueue_Func)
+b1=tk.Button(tab2, text="Call Log",bg="white",command=openCallLogFunc)
+b2=tk.Button(tab2, text="Complaints Log",bg="white",command=openComplaintsFunc)
+b3=tk.Button(tab2, text="Queue Log",bg="white",command=openQueue_Func)
 b1.place(x=310,y=120)
 b2.place(x=310,y=170)
 b3.place(x=310,y=220)
